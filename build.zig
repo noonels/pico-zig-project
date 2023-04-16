3bb5cc1bb05e0bcd4993be3c931d5feaf084643d
M.Cooper Healy <m.cooper.healy@gmail.com>
2023-04-16 13:15:39 -0500

    rp

--- build.zig
+++ build.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-//const uf2 = @import("main.zig");
+const uf2 = @import("deps/uf2/src/main.zig");
 const rp2040 = @import("deps/raspberrypi-rp2040/build.zig");
 
 // the hardware support package should have microzig as a dependency
@@ -8,7 +8,7 @@ const microzig = @import("deps/raspberrypi-rp2040/deps/microzig/build.zig");
 pub fn build(b: *std.build.Builder) !void {
     const optimize = b.standardOptimizeOption(.{});
     var exe = microzig.addEmbeddedExecutable(b, .{
-        .name = "firmware.uf2",
+        .name = "firmware",
         .source_file = .{
             .path = "src/main.zig",
         },
@@ -21,4 +21,7 @@ pub fn build(b: *std.build.Builder) !void {
         .optimize = optimize,
     });
     exe.installArtifact(b);
+
+    const uf2_step = uf2.Uf2Step.create(exe.inner, .{ .family_id = .RP2040 });
+    uf2_step.install();
 }
