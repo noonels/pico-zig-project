b9290fe78fa18ea1eaf25a47d98b555e9df34968
M.Cooper Healy <m.cooper.healy@gmail.com>
2023-04-16 14:15:48 -0500

    Add doc comments to explain use of firmware

--- src/main.zig
+++ src/main.zig
@@ -9,9 +9,25 @@ const pin_config = rp2040.pins.GlobalConfiguration{
     },
 };
 
+/// Firmware for Isomatter::Labs battlebot
+/// submitted to the 2024 Battlebots competition
+/// in the ant-weight division.
+///
+/// To build, run:
+/// `zig build`, and flash the resulting `zig-out/bin/firmware.uf2` file
+/// to the RP2040 using the UF2 interface.
 pub fn main() !void {
     const pins = pin_config.apply();
 
+    // currently, just blinks the LED as a proof of concept.
+    //   ideally, in the future, this will be the firmware for the main driver board,
+    //   which will control locomotion, remote control, and using the main weapon.
+
+    //   all other functionality will be handled by the peripheral boards, which will
+    //   have their own firmware, and will communicate with the main board over one-way
+    //   serial (main board -> peripheral boards). This is to provide redundancy,
+    //   and to prevent the possibility of having some peripheral function block the
+    //   primary control loop.
     while (true) {
         pins.led.toggle();
         time.sleep_ms(500);
